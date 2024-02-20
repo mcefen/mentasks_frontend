65797e8370172a1fd5c15dfb474ba74cea7037ea
viz13mor <mcefen@users.noreply.github.com>
2024-02-20 19:58:48 +0100

    Update ModalFormularioTarea.jsx

--- src/components/ModalFormularioTarea.jsx
+++ src/components/ModalFormularioTarea.jsx
@@ -1,5 +1,5 @@
 import { Fragment, useState, useEffect } from "react";
-import { Dialog, Transition } from "@headlessui/react";
+import { Dialog, Transition } from '../../../@headlessui/react/dist'
 import useProyectos from "../hooks/useProyectos";
 import Alerta from "./Alerta";
 import { useParams } from "react-router-dom";
