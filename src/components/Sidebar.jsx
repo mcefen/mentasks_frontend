7a890e0a1280092d6e4819e5fb182f0cc48e7657
viz13mor <mcefen@users.noreply.github.com>
2024-02-20 20:37:19 +0100

    Update Sidebar.jsx
    yo comabio

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import useAuth from "../hooks/useAuth"
 const Sidebar = () => {
 
   const { auth } = useAuth()
-  console.log(auth)
+  
   return (
     <aside className="md:w-1/3 lg:w-1/5 xl:w-1/6 px-5 py-10">
       <p className="text-xl font-bold">Hola:{auth.nombre} </p>
